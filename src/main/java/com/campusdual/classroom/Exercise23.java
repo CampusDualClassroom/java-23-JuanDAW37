adde83488a3f37f0ae9ffc41e805978db0d91b5f
JuanDAW37 <jymtr1968@gmail.com>
2024-10-03 10:10:40 +0200

    Exercicio 23 listo

--- src/main/java/com/campusdual/classroom/Exercise23.java
+++ src/main/java/com/campusdual/classroom/Exercise23.java
@@ -1,20 +1,39 @@
 package com.campusdual.classroom;
 
+import javax.print.attribute.standard.PrinterLocation;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class Exercise23 {
 
-    public static Map<String, Person> createHashMap() {
+    public static Map<String, Person> hashMap = new HashMap<>();
 
+    public static Map<String, Person> createHashMap() {
+        Person person = new Person();
+        person = addMapValue(hashMap, "person", new Person("John", "Smith"));
+        person = addMapValue(hashMap, "teacher", new Teacher("María", "Montessori", "Educación"));
+        person = addMapValue(hashMap, "police", new PoliceOfficer("Jake", "Peralta", "B-99"));
+        person = addMapValue(hashMap, "doctor",new Doctor("Gregory", "House", "Nefrología e infectología"));
+        return hashMap;
     }
 
     public static Person addMapValue(Map<String, Person> map, String key, Person value) {
-
+        map.put(key, value);
+        return hashMap.get("police");
     }
 
     public static void printMapValues(Map<String, Person> map) {
+        for(Map.Entry<String, Person> datos: map.entrySet()){
+            System.out.print("Key: " + datos.getKey() + " ");
+            datos.getValue().getDetails();
+        }
+    }
 
+    public static void main(String[] args){
+        hashMap = createHashMap();
+        Person person = addMapValue(hashMap, "police", new PoliceOfficer("Charles", "Boyle", "B-99"));
+        printMapValues(hashMap);
     }
     
 }
--- src/main/java/com/campusdual/classroom/Person.java
+++ src/main/java/com/campusdual/classroom/Person.java
@@ -9,6 +9,8 @@ public class Person {
 		this.surname = surname;
 	}
 
+	public Person(){}
+
 	public String getName() {
 		return this.name;
 	}
